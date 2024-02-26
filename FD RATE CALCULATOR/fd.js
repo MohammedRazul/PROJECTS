7cc06924aafd5026b5c20d543d288f8d562b1b1f
MohammedRazul <160466246+MohammedRazul@users.noreply.github.com>
2024-02-27 01:18:54 +0530

    Add files via upload

---
+++ FD RATE CALCULATOR/fd.js
@@ -0,0 +1,18 @@
+//FD RATE CALCULATOR
+
+
+function fd_rate(){
+  const months=Number(document.getElementById("type").value)
+  let rate=0
+
+  if(months <3)
+   rate=5.8
+  else if(months >=3 && months <6)
+   rate=6.5
+  else if(months >=7 && months <9)
+   rate=6.8
+  else
+   rate=7
+  let result=(document.getElementById("result"))
+  result.innerHTML="your interest rate is"+ rate +"%"
+}
